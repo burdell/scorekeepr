ace92030101186ead31a72655b20cd04273c122e
Nathan Griffin <nathanjgriffin@gmail.com>
2020-07-12 18:14:56 -0400

    finish up batter actions

--- lib/retrosheet/atBats.ts
+++ lib/retrosheet/atBats.ts
@@ -2,13 +2,57 @@ import { Scorekeeper } from '../Scorekeeper'
 import { handleOut, getMultiActionOut } from './outs'
 
 const isNumber = (str: string) => !isNaN(Number(str))
-const getFieldersChoice = (batterAction: string) =>
-  batterAction.match(/(\d+)\(\d\)/)
+const getFieldersChoice = (batterAction: string) => {
+  return batterAction.match(/^FC\d+/)
+}
+const isError = (atBatResult: string) => atBatResult.match(/C?\/?E(\d)/)
+const isHomerun = (atBatResult: string) => atBatResult.match(/^HR?\d?/)
+const isHitBatter = (atBatResult: string) => atBatResult.match(/^HP/)
+const isIntentionalWalk = (atBatResult: string) => atBatResult.match(/^IW?/)
+const isHit = (atBatResult: string) => {
+  if (atBatResult.match(/^DGR/)) return 2
+
+  const hitMatch = atBatResult.match(/^([S|D|T])\d+/)
+
+  if (hitMatch) {
+    const [_, hitType] = hitMatch
+    if (hitType === 'S') return 1
+    if (hitType === 'D') return 2
+    if (hitType === 'T') return 3
+  }
+}
 
 function handleBatterAction(atBatResult: string, game: Scorekeeper) {
+  const errorMatch = isError(atBatResult)
+  if (errorMatch) {
+    const [_, defensivePosition] = errorMatch
+    game.defensiveError({
+      defensivePlayer: Number(defensivePosition),
+      baseAdvancedTo: 1
+    })
+
+    return
+  }
+
   const [batterAction, ...modifiers] = atBatResult.split('/')
 
-  if (batterAction === 'HR') {
+  const hit = isHit(batterAction)
+  if (hit) {
+    game.hit(hit)
+    return
+  }
+
+  if (isHitBatter(batterAction)) {
+    game.hitBatter()
+    return
+  }
+
+  if (isIntentionalWalk(batterAction)) {
+    game.intentionalWalk()
+    return
+  }
+
+  if (isHomerun(batterAction)) {
     game.hit(4)
     return
   }
@@ -19,15 +63,19 @@ function handleBatterAction(atBatResult: string, game: Scorekeeper) {
     return
   }
 
+  const fieldersChoice = getFieldersChoice(batterAction)
+  if (fieldersChoice) {
+    game.fieldersChoice()
+    return
+  }
+
   const { batter } = getMultiActionOut(batterAction)
   if (batter) {
     handleOut(batter, atBatResult, game)
     return
-  }
-
-  const fieldersChoice = getFieldersChoice(batterAction)
-  if (fieldersChoice) {
+  } else {
     game.fieldersChoice()
+    return
   }
 }
 
